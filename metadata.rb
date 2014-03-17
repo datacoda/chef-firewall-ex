5a32808f17a7663ef177b4a14479265056a94601
Ted Chen <ted@nephilagraphic.com>
2014-03-17 01:28:43 -0300

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name             'networking'
+maintainer       'Nephila Graphic'
+maintainer_email 'ted@nephilagraphic.com'
+license          'Apache 2.0'
+description      'Installs/Configures networking'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+supports 'ubuntu'
+
+depends 'firewall'
+
